06db4349781064915708050d516a9170f74f892c
DenisGnamm <denisgnamm@.gmail.com>
2023-03-28 22:14:01 +0500

    Added seminar1 example2

--- seminar1/Program.cs
+++ seminar1/Program.cs
@@ -6,3 +6,24 @@ Console.Write("Введите число B: "); int B = int.Parse(Console.ReadLi
 if (A>B) Console.Write($"{A}"+" больше чем "+$"{B}");
 
 else Console.Write($"{B}"+" больше чем "+$"{A}");
+
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Write("Введите число C: "); int C = int.Parse(Console.ReadLine()!); 
+
+Console.Write("Введите число D: "); int D = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите число E: "); int E = int.Parse(Console.ReadLine()!);
+
+int count = 0; int max = 0;
+
+while (count < 2)
+{
+if (C > max) max = C;
+if (D > max) max = D;
+if (E > max) max = E;
+count++;
+};
+
+Console.Write("Наибольшее - "+$"{max}");
