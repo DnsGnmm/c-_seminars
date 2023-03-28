f69f108e6aac90b2d211b2eebcdee356c119fc6a
DenisGnamm <denisgnamm@.gmail.com>
2023-03-28 21:45:07 +0500

    Added seminar1 example1

---
+++ seminar1/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.Write("Введите число A: "); int A = int.Parse(Console.ReadLine()!); 
+
+Console.Write("Введите число B: "); int B = int.Parse(Console.ReadLine()!);
+
+if (A>B) Console.Write($"{A}"+" больше чем "+$"{B}");
+
+else Console.Write($"{B}"+" больше чем "+$"{A}");
